767be80b139222dd37c312425e39ab5fd23bac8e
Matteo Bortolazzo <bortolazzo.matteo@gmail.com>
2019-03-22 12:18:34 +0000

    Refactor MethodCallExpressionTranslator.

--- src/CouchDB.Client/Translators/MethodCallExpressionTranslator.cs
+++ src/CouchDB.Client/Translators/MethodCallExpressionTranslator.cs
@@ -17,105 +17,32 @@ namespace CouchDB.Client
         protected override Expression VisitMethodCall(MethodCallExpression m)
         {
             // Queryable
-            if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "Where")
+            if (m.Method.DeclaringType == typeof(Queryable))
             {
-                return VisitWhereMethod(m);
-            }
-            else if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "OrderBy")
-            {
-                return VisitOrderByMethod(m, true);
-            }
-            else if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "ThenBy")
-            {
-                return VisitOrderByMethod(m, true);
-            }
-            else if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "OrderByDescending")
-            {
-                return VisitOrderByMethod(m, false);
-            }
-            else if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "ThenByDescending")
-            {
-                return VisitOrderByMethod(m, false);
-            }
-            else if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "Skip")
-            {
-                return VisitSkipMethod(m);
-            }
-            else if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "Take")
-            {
-                return VisitTakeMethod(m);
-            }
-            else if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "Select")
-            {
-                return VisitSelectMethod(m);
+                if (m.Method.Name == "Where")
+                    return VisitWhereMethod(m);
+                else if (m.Method.Name == "OrderBy" || m.Method.Name == "ThenBy")
+                    return VisitOrderAscendingMethod(m);
+                else if (m.Method.Name == "OrderByDescending" || m.Method.Name == "ThenByDescending")
+                    return VisitOrderDescendingMethod(m);
+                else if (m.Method.Name == "Skip")
+                    return VisitSkipMethod(m);
+                else if (m.Method.Name == "Take")
+                    return VisitTakeMethod(m);
+                else if (m.Method.Name == "Select")
+                    return VisitSelectMethod(m);
             }
             // Not Queryable
-            else if (m.Method.DeclaringType != typeof(Queryable) && m.Method.Name == "All")
+            else
             {
-                return VisitAnyMethod(m);
-            }
-            else if (m.Method.DeclaringType != typeof(Queryable) && m.Method.Name == "Any")
-            {
-                return VisitAllMethod(m);
+                if (m.Method.Name == "All")
+                    return VisitAnyMethod(m);
+                else if (m.Method.Name == "Any")
+                    return VisitAllMethod(m);
             }
 
             throw new NotSupportedException(string.Format("The method '{0}' is not supported", m.Method.Name));
-        }
-
-        private Expression VisitOrderByMethod(MethodCallExpression m, bool ascending)
-        {
-            void InspectOrdering(Expression e)
-            {
-                var o = e as MethodCallExpression;
-                if (o.Method.DeclaringType != typeof(Queryable))
-                    throw new NotSupportedException(string.Format("The method '{0}' is not supported", o.Method.Name));
-                
-                var lambda = (LambdaExpression)StripQuotes(o.Arguments[1]);
-                if (o.Method.Name == "OrderBy")
-                {
-                    this.Visit(o.Arguments[0]);
-                    if (!ascending)
-                        throw new InvalidOperationException("Cannot order in different directions.");
-                    sb.Append("\"sort\":[");
-                    this.Visit(lambda.Body);
-                }
-                else if (o.Method.Name == "OrderByDescending")
-                {
-                    this.Visit(o.Arguments[0]);
-                    if (ascending)
-                        throw new InvalidOperationException("Cannot order in different directions.");
-                    sb.Append("\"sort\":[");
-                    sb.Append("{");
-                    this.Visit(lambda.Body);
-                    sb.Append(":\"desc\"}");
-                }
-                else if (o.Method.Name == "ThenBy")
-                {
-                    if (!ascending)
-                        throw new InvalidOperationException("Cannot order in different directions.");
-                    InspectOrdering(o.Arguments[0]);
-                    this.Visit(lambda.Body);
-                }
-                else if (o.Method.Name == "ThenByDescending")
-                {
-                    if (ascending)
-                        throw new InvalidOperationException("Cannot order in different directions.");
-                    InspectOrdering(o.Arguments[0]);
-                    sb.Append("{");
-                    this.Visit(lambda.Body);
-                    sb.Append(":\"desc\"}");
-                }
-                else
-                {
-                    return;
-                }
-                sb.Append(",");
-            }
-
-            InspectOrdering(m);
-            sb.Append("],");
-            return m;
-        }
+        }              
 
         #region Queryable
 
@@ -126,6 +53,72 @@ namespace CouchDB.Client
             var lambda = (LambdaExpression)StripQuotes(m.Arguments[1]);
             this.Visit(lambda.Body);
             sb.Append(",");
+            return m;
+        }
+        private Expression VisitOrderAscendingMethod(MethodCallExpression m)
+        {
+            void InspectOrdering(Expression e)
+            {
+                var o = e as MethodCallExpression;
+                var lambda = (LambdaExpression)StripQuotes(o.Arguments[1]);
+
+                if (o.Method.Name == "OrderBy")
+                {
+                    this.Visit(o.Arguments[0]);
+                    sb.Append("\"sort\":[");
+                    this.Visit(lambda.Body);
+                }
+                else if (o.Method.Name == "OrderByDescending")
+                {
+                    throw new InvalidOperationException("Cannot order in different directions.");
+                }
+                else if (o.Method.Name == "ThenBy")
+                {
+                    InspectOrdering(o.Arguments[0]);
+                    this.Visit(lambda.Body);
+                }
+                else
+                    return;
+                sb.Append(",");
+            }
+
+            InspectOrdering(m);
+            sb.Append("],");
+            return m;
+        }
+        private Expression VisitOrderDescendingMethod(MethodCallExpression m)
+        {
+            void InspectOrdering(Expression e)
+            {
+                var o = e as MethodCallExpression;
+                var lambda = (LambdaExpression)StripQuotes(o.Arguments[1]);
+
+                if (o.Method.Name == "OrderBy")
+                {
+                    throw new InvalidOperationException("Cannot order in different directions.");
+                }
+                else if (o.Method.Name == "OrderByDescending")
+                {
+                    this.Visit(o.Arguments[0]);
+                    sb.Append("\"sort\":[");
+                    sb.Append("{");
+                    this.Visit(lambda.Body);
+                    sb.Append(":\"desc\"}");
+                }
+                else if (o.Method.Name == "ThenByDescending")
+                {
+                    InspectOrdering(o.Arguments[0]);
+                    sb.Append("{");
+                    this.Visit(lambda.Body);
+                    sb.Append(":\"desc\"}");
+                }
+                else
+                    return;
+                sb.Append(",");
+            }
+
+            InspectOrdering(m);
+            sb.Append("],");
             return m;
         }
         private Expression VisitSkipMethod(MethodCallExpression m)
