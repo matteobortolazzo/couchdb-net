f764cc35aa89dca62bf65097f709365f6d4e110f
Ben Origas <borigas@ocuvera.com>
2019-03-22 12:57:07 -0500

    DataMember for CouchEntity

--- src/CouchDB.Client/CouchEntity.cs
+++ src/CouchDB.Client/CouchEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 using Newtonsoft.Json;
 
@@ -7,13 +8,17 @@ namespace CouchDB.Client
 {
     public abstract class CouchEntity
     {
+        [DataMember]
         [JsonProperty("_id", NullValueHandling = NullValueHandling.Ignore)]
         public string Id { get; set; }
+        [DataMember]
         [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
         private string IdOther { set => Id = value; }
 
+        [DataMember]
         [JsonProperty("_rev", NullValueHandling = NullValueHandling.Ignore)]
         public string Rev { get; set; }
+        [DataMember]
         [JsonProperty("rev", NullValueHandling = NullValueHandling.Ignore)]
         private string RevOther { set => Rev = value; }
     }
