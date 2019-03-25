63584789a4b597266c10b1ed4d724689e2c0a462
Matteo Bortolazzo <bortolazzo.matteo@gmail.com>
2019-03-25 15:01:05 +0000

    Added test for authentication.

--- src/CouchDB.Driver/CouchClient.cs
+++ src/CouchDB.Driver/CouchClient.cs
@@ -2,11 +2,17 @@
 using CouchDB.Driver.Types;
 using Flurl.Http;
 using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace CouchDB.Driver
 {
     public class CouchClient : IDisposable
     {
+        private DateTime? cookieCreationDate;
+        private string cookieToken;
         private readonly CouchSettings settings;
         private readonly FlurlClient flurlClient;
         public string ConnectionString { get; private set; }
@@ -18,8 +24,9 @@ namespace CouchDB.Driver
 
             ConnectionString = connectionString;
             flurlClient = new FlurlClient(connectionString);
+            flurlClient.Configure(s => s.BeforeCall = OnBeforeLogin);
             settings = new CouchSettings();
-            configFunc?.Invoke(settings);
+            configFunc(settings);            
         }
         public CouchDatabase<TSource> GetDatabase<TSource>() where TSource : CouchEntity
         {
@@ -34,10 +41,64 @@ namespace CouchDB.Driver
 
             return new CouchDatabase<TSource>(flurlClient, ConnectionString, db);
         }
-
         public void Dispose()
         {
             flurlClient.Dispose();
+        }
+
+        private async Task Login()
+        {
+            var response = await flurlClient.Request(ConnectionString)
+                .AppendPathSegment("_session")
+                .PostJsonAsync(new
+                {
+                    name = settings.Username,
+                    password = settings.Password
+                });
+
+            cookieCreationDate = DateTime.Now;
+
+            if (response.Headers.TryGetValues("Set-Cookie", out var values))
+            {
+                var dirtyToken = values.First();
+                var regex = new Regex(@"^AuthSession=(.+); Version=1; .*Path=\/; HttpOnly$");
+                var match = regex.Match(dirtyToken);
+                if (match.Success)
+                {
+                    cookieToken = match.Groups[1].Value;
+                    return;
+                }
+            }
+
+            throw new InvalidOperationException("Error while trying to log-in.");
+        }
+        private void OnBeforeLogin(HttpCall call)
+        {
+            // If cookie request
+            if (call.Request.RequestUri.ToString().Contains("_session") && call.Request.Method == HttpMethod.Post)
+            {
+                return;
+            }
+            switch (settings.AuthenticationType)
+            {
+                case AuthenticationType.None:
+                    break;
+                case AuthenticationType.Basic:
+                    call.FlurlRequest.WithBasicAuth(settings.Username, settings.Password);
+                    break;
+                case AuthenticationType.Cookie:
+                    var isTokenExpired = 
+                        !cookieCreationDate.HasValue || 
+                        cookieCreationDate.Value.AddMinutes(settings.CookiesDuration) < DateTime.Now;
+                    if (isTokenExpired)
+                    {
+                        Login().Wait();
+                    }
+                    call.FlurlRequest.EnableCookies().WithCookie("AuthSession", cookieToken);
+                    break;
+                default:
+                    throw new NotSupportedException($"Authentication of type {settings.AuthenticationType} is not supported.");
+            }
         }
     }
 }
---
+++ tests/CouchDB.Driver.UnitTests/Requests_Test.cs
@@ -0,0 +1,71 @@
+﻿using CouchDB.Driver.UnitTests.Models;
+using Flurl.Http.Testing;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CouchDB.Driver.UnitTests
+{
+    public class Requests_Test
+    {
+        [Fact]
+        public async Task Authentication_None()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest.RespondWithJson(new { Docs = new string[0] });
+
+                var client = new CouchClient("http://localhost:5984", s => s.ConfigureBasicAuthentication("root", "relax"));
+                var rebels = client.GetDatabase<Rebel>();
+                var all = await rebels.ToListAsync();
+
+                httpTest
+                    .ShouldHaveCalled("http://localhost:5984/rebels/_find")
+                    .WithVerb(HttpMethod.Post);
+            }
+        }
+        [Fact]
+        public async Task Authentication_Basic()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest.RespondWithJson(new { Docs = new string[0] });
+
+                var client = new CouchClient("http://localhost:5984", s => s.ConfigureBasicAuthentication("root", "relax"));
+                var rebels = client.GetDatabase<Rebel>();
+                var all = await rebels.ToListAsync();
+
+                httpTest
+                    .ShouldHaveCalled("http://localhost:5984/rebels/_find")
+                    .WithVerb(HttpMethod.Post)
+                    .WithBasicAuth("root", "relax");
+            }
+        }
+        [Fact]
+        public async Task Authentication_Cookie()
+        {
+            var token = "cm9vdDo1MEJCRkYwMjq0LO0ylOIwShrgt8y-UkhI-c6BGw";
+
+            using (var httpTest = new HttpTest())
+            {
+                var cookieResponse = new HttpResponseMessage();
+                cookieResponse.Headers.Add("Content-Typ", "application/json");
+                cookieResponse.Headers.Add("Set-Cookie", $"AuthSession={token}; Version=1; Path=/; HttpOnly");
+                cookieResponse.Content = new StringContent("{}");
+                httpTest.ResponseQueue.Enqueue(cookieResponse);
+                httpTest.RespondWithJson(new { Docs = new string[0] });
+
+                var client = new CouchClient("http://localhost:5984", s => s.ConfigureCookieAuthentication("root", "relax"));
+                var rebels = client.GetDatabase<Rebel>();
+                var all = await rebels.ToListAsync();
+
+                var authCookie = httpTest.CallLog
+                    .Single(c => c.Request.RequestUri.ToString().Contains("_session"))
+                    .FlurlRequest.Cookies.Single(c => c.Key == "AuthSession").Value;
+                Assert.Equal(token, authCookie.Value);
+            }
+        }
+    }
+}
