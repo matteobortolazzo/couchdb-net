776ad76cba9c44a01b6598a923a191cd8208dd6b
Matteo Bortolazzo <bortolazzo.matteo@gmail.com>
2019-03-27 12:39:48 +0000

    Added couchType documentation.

--- src/CouchDB.Driver/Types/CouchType.cs
+++ src/CouchDB.Driver/Types/CouchType.cs
@@ -5,6 +5,9 @@
     /// </summary>
     public class CouchType
     {
+        /// <summary>
+        /// Represents the CouchDB type value.
+        /// </summary>
         public string Value { get; }
 
         private CouchType(string value)
@@ -12,11 +15,29 @@
             Value = value;
         }
 
+        /// <summary>
+        /// Represents the "null" type.
+        /// </summary>
         public static readonly CouchType Null = new CouchType("null");
+        /// <summary>
+        /// Represents the "boolean" type.
+        /// </summary>
         public static readonly CouchType Boolean = new CouchType("boolean");
+        /// <summary>
+        /// Represents the "number" type.
+        /// </summary>
         public static readonly CouchType Number = new CouchType("number");
+        /// <summary>
+        /// Represents the "string" type.
+        /// </summary>
         public static readonly CouchType String = new CouchType("string");
+        /// <summary>
+        /// Represents the "array" type.
+        /// </summary>
         public static readonly CouchType Array = new CouchType("array");
+        /// <summary>
+        /// Represents the "object" type.
+        /// </summary>
         public static readonly CouchType Object = new CouchType("object");
     }
 }
